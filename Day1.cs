df8669ab82a6873557a6ef4922a302e4700b6af2
mkujun <kujundzic.marko@hotmail.com>
2023-12-02 12:46:47 +0100

    day 1 part 1

---
+++ Day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Aoc
+{
+    public class Day1
+    {
+        //string input = "C:\\Users\\Korisnik\\Projekti\\AdventOfCode2023\\Inputs\\input.txt";
+        string input = "C:\\Users\\Korisnik\\Projekti\\AdventOfCode2023\\Inputs\\1.txt";
+
+        public Day1()
+        {
+            PartOne();
+        }
+
+        public void PartOne()
+        {
+            int sum = 0;
+
+            foreach (string line in File.ReadLines(@input))
+            {
+                string nums = new String(line.Where(Char.IsDigit).ToArray());
+
+                char first = nums[0];
+                char last = nums[nums.Length - 1];
+
+                string numString = first.ToString() + last.ToString();
+
+                sum = sum + Int32.Parse(numString);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Hello, World!");
+using Aoc;
+
+Day1 day1 = new Day1();
+
+
