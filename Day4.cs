3ca4d4d2c3c56e2bd974ac7974df7dc255c7c98d
mkujun <kujundzic.marko@hotmail.com>
2023-12-10 17:20:36 +0100

    day 4 part 2

--- Day4.cs
+++ Day4.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -13,8 +14,64 @@ namespace Aoc
     {
         //string input = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Inputs\input.txt"));
         string input = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Inputs\4.txt"));
+        Dictionary<int, int> myDict = new Dictionary<int, int>();
 
         public Day4()
+        {
+            //PartOne();
+            CreateDict();
+            PartTwo();
+        }
+
+        private void CreateDict()
+        {
+            for (int i = 1; i <= 208; i++)
+            {
+                myDict.Add(i, 0);
+            }
+        }
+
+        private void PartTwo()
+        {
+            int counter = 1;
+            int sum = 0;
+
+            foreach (string line in File.ReadLines(@input))
+            {
+                int copiesAtCounter = myDict[counter];
+
+                List<string> winningNumbers = GetWinningNumbersFromLine(line);
+                List<string> ticket = GetTicketFromLine(line);
+
+                var matchingItems = winningNumbers.Intersect(ticket).ToList();
+
+                for (int i = 0; i < matchingItems.Count; i++)
+                {
+                    myDict[i + counter + 1] = myDict[i + counter + 1] + 1;
+                }
+
+                for (int j = 0; j < copiesAtCounter; j++)
+                {
+                    for (int i = 0; i < matchingItems.Count; i++)
+                    {
+                        myDict[i + counter + 1] = myDict[i + counter + 1] + 1;
+                    }
+                }
+
+                counter++;
+            }
+
+
+            foreach (var key in myDict.Keys.ToList())
+            {
+                myDict[key] += 1;
+                sum = sum + myDict[key];    
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private void PartOne()
         {
             int part1 = 0;
 
@@ -33,11 +90,12 @@ namespace Aoc
                         point = point * 2;
                     }
 
-                part1 = part1 + point;
+                    part1 = part1 + point;
                 }
             }
 
             Console.WriteLine(part1);
+
         }
 
         private List<string> GetTicketFromLine(string line)
