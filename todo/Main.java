141edc712b53b35986c13d13f4c62c2b40c5f339
mkujun <kujundzic.marko@hotmail.com>
2024-08-08 10:01:22 +0200

    daz 7 wip

--- todo/Main.java
+++ todo/Main.java
@@ -1,6 +1,7 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -10,27 +11,19 @@ public class Main {
         List<Hand> handList = new ArrayList<>();
 
         for (String line : lines) {
-            Hand hand = readHand(line);
+            Hand hand = new Hand(line);
+            System.out.println(hand);
             if (handList.isEmpty()) {
                 handList.add(hand);
-            }
-            else {
+            } else {
                 // todo: read and compare with others in the list and set to appropriate index
                 insertHandIntoList(hand, handList);
             }
         }
     }
 
-    private static Hand readHand(String line) {
-        String[] parts = line.split(" ");
-        String value = parts[0];
-        String bid = parts[1];
-
-        return new Hand(value, bid);
-    }
-
     private static void insertHandIntoList(Hand hand, List<Hand> handList) {
-        for (int i = 0; i <handList.size() ; i++) {
+        for (int i = 0; i < handList.size(); i++) {
             int indexToInsert = findIndexToInsert(hand, handList.get(i));
         }
     }
@@ -45,9 +38,70 @@ public class Main {
 class Hand {
     private String value;
     private String bid;
+    private String type;
 
     public Hand(String value, String bid) {
         this.value = value;
         this.bid = bid;
+
+        parseType(value);
+    }
+
+    public Hand(String line) {
+        String[] parts = line.split(" ");
+
+        this.value = parts[0];
+        this.bid = parts[1];
+
+        parseType(value);
+    }
+
+    private void parseType(String str) {
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+        char[] charArray = str.toCharArray();
+
+        for (char c : charArray) {
+            if (charCountMap.containsKey(c)) {
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            } else {
+                charCountMap.put(c, 1);
+            }
+        }
+
+        this.type = defineType(charCountMap);
+    }
+
+    private String defineType(HashMap<Character, Integer> charMap) {
+        if (charMap.containsValue(5)) {
+            return "Five";
+        }
+        if (charMap.containsValue(4)) {
+            return "Four";
+        }
+        if (charMap.containsValue(3) && charMap.containsValue(2)) {
+            return "FullHouse";
+        }
+        if (charMap.containsValue(3) && !charMap.containsValue(2)) {
+            return "Three";
+        }
+
+        if (charMap.containsValue(2)) {
+            int size = charMap.size();
+
+            if (size == 4) {
+                return "OnePair";
+            }
+            if (size == 3) {
+                return "TwoPair";
+            }
+        } else if (charMap.containsValue(1)) {
+            return "HighCard";
+        }
+
+        return "UndefinedType";
+    }
+
+    public String toString() {
+        return "Value " + this.value + " Bid " + this.bid + " Type " + this.type;
     }
 }
